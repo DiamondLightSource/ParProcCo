30bb754a513875eff0ad65e25d932cda3b091855
Peter Chang <peter.chang@diamond.ac.uk>
2021-11-08 11:39:18 +0000

    Add pass-through mode for single job
    Refactor to pass fewer arguments, reduce attributes, encapsulate program specific slicer and modes

---
+++ ParProcCo/program_wrapper.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from .slicer_interface import SlicerInterface
+from .scheduler_mode_interface import SchedulerModeInterface
+
+class ProgramWrapper:
+
+    def __init__(self, slicer : SlicerInterface, processing_mode: SchedulerModeInterface, aggregating_mode: Optional[SchedulerModeInterface] = None):
+        self.slicer = slicer
+        self.processing_mode = processing_mode
+        self.aggregating_mode = aggregating_mode
+
+    def create_slices(self, number_jobs: int, stop: Optional[int] = None) -> List[slice]:
+        return self.slicer.slice(number_jobs, stop)
+
